96ebe4495db0507b486fee60b264c90df2fa1a85
Robert Mutai <rbmutai@gmail.com>
2024-03-13 12:39:18 +0300

    add mood details and logic

---
+++ MeCheck/MoodItem.swift
@@ -0,0 +1,8 @@
+//
+//  MoodItem.swift
+//  MeCheck
+//
+//  Created by Robert Mutai on 12/03/2024.
+//
+
+import Foundation
---
+++ MeCheck/MoodRow.swift
@@ -0,0 +1,18 @@
+//
+//  MoodRow.swift
+//  MeCheck
+//
+//  Created by Robert Mutai on 12/03/2024.
+//
+
+import SwiftUI
+
+struct MoodRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoodRow()
+}
