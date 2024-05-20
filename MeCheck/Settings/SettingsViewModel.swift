9f581c0d05fbee4acf610c2173c5b6fd9c9c499e
Robert Mutai <rbmutai@gmail.com>
2024-05-20 17:09:37 +0300

    update share detail

--- MeCheck/Settings/SettingsViewModel.swift
+++ MeCheck/Settings/SettingsViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 class SettingsViewModel: ObservableObject {
     
     private var appNavigation: AppNavigation
-    var shareInfo: String =  String(localized: "Hey, I use this great app called \(Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? "MeCheck"). It enables me to record my moods, set new habits and keep a gratitude journal. You can get it on the appstore at https://appstore.com/ ")
+    var shareInfo: String =  String(localized: "Hey, I use this great app called \(Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String ?? "MeCheck"). It enables me to record my moods, set new habits and keep a gratitude journal. You can get it on the appstore at https://appstore.com/ ")
     let freeLabel = String(localized: "Free Version")
     let premiumLabel = String(localized: "Premium Version")
     init(appNavigation: AppNavigation) {
